c2cef5816fe51c66d1809a3921be7717d13d4e6d
suwan <jsuwan961205@gmail.com>
2021-12-13 11:05:35 +0900

    reformat files

---
+++ src/gateway/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request
+from dispatch import call_dispatcher
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Welcome to api gateway"
+
+@app.route("/run_deploy_pipeline", methods=["POST"])
+def run_deploy_pipepline():
+    data = request.form.get("text")
+    # print(data)
+    data = {info.split("-")[0]: info.split("-")[1] for info in data.split()}
+    print("data: ", data)
+    call_dispatcher(data)
+    return f"배포 파이프라인이 실행되었습니다.\n https://github.com/ssuwani/deploy-pipeline/actions\n 실행된 하이퍼 파라미터: {data}"
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run(host="0.0.0.0", port=5000)
--- src/gateway/dispatch.py
+++ src/gateway/dispatch.py
@@ -3,26 +3,34 @@ import requests
 import json
 
 
-TOKEN = "<GITHUB ACCESS TOKEN>"
-OWNER = "KIMnJANG"
-REPO = "mnist-model"
+TOKEN = os.getenv("ACCESS_TOKEN")
+OWNER = "ssuwani"
+REPO = "deploy-pipeline"
 
-WORKFLOW_ID = "deploy.yml"
+WORKFLOW_ID = "ci.yml"
 headers = {
     "Authorization": f"token {TOKEN}",
 }
 
 
-def call_dispatcher(hpo):
-    units = hpo["hidden_units"]
-    optimizer = hpo["optimizer"]
-    print(units, optimizer)
-    data = {"ref": "main", "inputs": {"units": str(units), "optimizer": optimizer}}
+def call_dispatcher(data):
+    units = data["hidden_units"]
+    optimizer = data["optimizer"]
+    version = data["version"]
+    print(units, optimizer, version)
+    data_json = {
+        "ref": "master",
+        "inputs": {
+            "hidden_units": str(units),
+            "optimizer": optimizer,
+            "version": version,
+        },
+    }
     # data = {"ref": "main", "inputs":{"model_path": model_path, "model_tag": model_tag }}
     res = requests.post(
         f"https://api.github.com/repos/{OWNER}/{REPO}/actions/workflows/{WORKFLOW_ID}/dispatches",
         headers=headers,
-        data=json.dumps(data),
+        data=json.dumps(data_json),
     )
     print(res.text)
     print(res.status_code)
